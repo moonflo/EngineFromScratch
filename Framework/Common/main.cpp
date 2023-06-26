47555d950e54e6fb0d3d304c76a26535e1352bef
Xuepu Zeng <2307665474zxp@gmail.com>
2023-06-26 18:52:51 +0800

    Update main.cpp

--- Framework/Common/main.cpp
+++ Framework/Common/main.cpp
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include "IApplication.hpp"
 
-using namespace My;
-
 namespace My {
+// Global varience, a pointer of an application
 extern IApplication* g_pApp;
-}
+}  // namespace My
 
 int main(int argc, char** argv) {
     int ret;
 
-    if ((ret = g_pApp->Initialize()) != 0) {
+    if ((ret = My::g_pApp->Initialize()) != 0) {
         printf("App Initialize failed, will exit now.");
         return ret;
     }
 
-    while (!g_pApp->IsQuit()) {
-        g_pApp->Tick();
+    while (!My::g_pApp->IsQuit()) {
+        My::g_pApp->Tick();
     }
 
-    g_pApp->Finalize();
+    My::g_pApp->Finalize();
 
     return 0;
 }
+// In this branch, set floder-common's files as lib by using "add_libraries" in
+// cmakelists.txt, and when generate exe in empty, we link this lib file.
