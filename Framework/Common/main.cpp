734ad1b974acd0ba1599746378709ba22109c6be
Xuepu Zeng <2307665474zxp@gmail.com>
2023-06-26 18:52:07 +0800

    update main.cpp
    总结第一次的各种设计的意义和心得，作为注释标注在main函数中了

--- Framework/Common/main.cpp
+++ Framework/Common/main.cpp
@@ -1,25 +1,33 @@
 #include <stdio.h>
 #include "IApplication.hpp"
 
-using namespace My;
-
 namespace My {
+// Global varience, a pointer of an application
 extern IApplication* g_pApp;
-}
+}  // namespace My
 
 int main(int argc, char** argv) {
     int ret;
 
-    if ((ret = g_pApp->Initialize()) != 0) {
+    if ((ret = My::g_pApp->Initialize()) != 0) {
         printf("App Initialize failed, will exit now.");
         return ret;
     }
 
-    while (!g_pApp->IsQuit()) {
-        g_pApp->Tick();
+    while (!My::g_pApp->IsQuit()) {
+        My::g_pApp->Tick();
     }
 
-    g_pApp->Finalize();
+    My::g_pApp->Finalize();
 
     return 0;
 }
+// In this branch, set floder-common's files as lib by using "add_libraries" in
+// cmakelists.txt, and when generate exe in empty, we link this lib file.
+
+// When configuring cmake for the first time, we configure the folder containing
+// main as a static library, and compile only the class declaration in Empty
+// into an executable program without any running code. Static libraries should
+// usually be set to fixed code that does not need to be changed, and this is
+// indeed the case in common, but it should be noted that usually we still
+// compile the program containing main into an executable program.
