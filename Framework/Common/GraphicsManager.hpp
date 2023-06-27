bd5d5f00518c0c200839c1e14c4c4cdbf0673d77
Xuepu Zeng <2307665474zxp@gmail.com>
2023-06-27 16:31:37 +0800

    read graphic, add some windows creating scripts

---
+++ Framework/Common/GraphicsManager.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "IRuntimeModule.hpp"
+namespace My {
+class GraphicsManager : implements IRuntimeModule {
+   public:
+    virtual ~GraphicsManager() {}
+};
+}  // namespace My
