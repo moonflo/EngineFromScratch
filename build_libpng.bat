b01c911ddbe35411bacb850e536f1eee4b849eb1
Xuepu Zeng <2307665474zxp@gmail.com>
2023-07-08 22:28:50 +0800

    delete bat file

--- build_libpng.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-git submodule update --init External/src/libpng
-mkdir -p External\build\libpng
-cd External\build\libpng
-cmake -DCMAKE_INSTALL_PREFIX=../../Windows -G "NMake Makefiles" ../../src/libpng
-cmake --build . --config release --target install
-
-# Visual Studio 17 2022 Win64
-
