951e8e1ae55bc0a9add74e3007bcb7b9b6a8fc1a
Xuepu Zeng <2307665474zxp@gmail.com>
2023-07-10 13:31:27 +0800

    add build_all

--- bat/build_libpng.bat
+++ bat/build_libpng.bat
@@ -1,7 +1,8 @@
-git submodule update --init External/src/libpng
-mkdir External\build\libpng
-cd External\build\libpng
-del -rf *
+git submodule update --init ..\External\src\libpng
+mkdir ..\External\build\libpng
+pushd ..\External\build\libpng
+del /f /s /q * >nul
 cmake -DCMAKE_PREFIX_PATH="../../Windows" -DCMAKE_INSTALL_PREFIX=..\..\windows -G "Visual Studio 17 2022" -Thost=x64 ..\..\src\libpng
 cmake --build . --config debug --target install
-pause
+popd
+
